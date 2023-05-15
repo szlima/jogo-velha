47106434a8f30195fcfdc27d93c20ef7b3554506
szlima <suzanelima@outlook.com>
2023-05-15 11:58:54 -0300

    Armazenando um histórico de movimentos

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function Square({value, onSquareClick}){
     );
 }
 
-export default function Board(){
-    const [xIsNext, setXIsNext]= useState(true);
-    const [squares, setSquares]= useState(Array(9).fill(null));
+function Board({xIsNext, squares, onPlay}){
     
     function handleClick(i){
 
@@ -25,8 +23,7 @@ export default function Board(){
         else
             nextSquares[i]= 'O';
 
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext);
+        onPlay(nextSquares);
     }
 
     const winner= calculateWinner(squares);
@@ -59,6 +56,30 @@ export default function Board(){
     );
 }
 
+export default function Game(){
+    const [xIsNext, setXIsNext]= useState(true);
+    const [history, setHistory]= useState([Array(9).fill(null)]);
+    const currentSquares= history[history.length-1];
+
+    function handlePlay(nextSquares){
+        setHistory([...history, nextSquares]);
+        setXIsNext(!xIsNext);
+    }
+
+    return (
+        <div className='game'>
+            <div className='game-board'>
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+            </div>
+            <div className='game-info'>
+                <ol>
+
+                </ol>
+            </div>
+        </div>
+    );
+}
+
 function calculateWinner(squares){
 
     const lines= [
@@ -80,3 +101,9 @@ function calculateWinner(squares){
     }
     return null;
 }
+
+/*
+No React, é convencional usar nomes 
+onSomething para props que representam eventos e 
+handleSomething para as definições das funções que lidam com esses eventos.
+*/
